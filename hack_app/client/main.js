9dc11b4b3d54763e16beb72795eff65eeded7774
Ziqi Chen <zqchen@linkedin.com>
2017-07-15 03:35:09 -0700

    logic woot

--- hack_app/client/main.js
+++ hack_app/client/main.js
@@ -1,22 +1,34 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import './main.html';
 
 Template.mainScreen.onCreated(function helloOnCreated() {
-  // counter starts at 0
+  	// counter starts at 0
   this.counter = new ReactiveVar(0);
+  var self = this;
+
+  self.vars = new ReactiveDict();
+
+  self.vars.setDefault( 'clicked' , false);
 });
 
 Template.mainScreen.helpers({
   counter() {
     return Template.instance().counter.get();
   },
+  clicked:function(){
+	 var instance = Template.instance(); //http://docs.meteor.com/#/full/template_instance
+	 return  instance.vars.get('clicked') //this will return false(default) | true
+	}
 });
 
 Template.mainScreen.events({
-  'click button'(event, instance) {
+  'click .blue'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    console.log("boob");
+    instance.vars.set('clicked', true);
   },
+  
 });
